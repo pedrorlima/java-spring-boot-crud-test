e5002f242562cc20f6d1cd007a13bb282672ec3a
Pedro <024.prlima@gmail.com>
2025-12-02 19:50:33 -0300

    initial setup

---
+++ src/main/java/com/example/java_spring_boot_crud_test/JavaSpringBootCrudTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.java_spring_boot_crud_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringBootCrudTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringBootCrudTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/java_spring_boot_crud_test/JavaSpringBootCrudTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.java_spring_boot_crud_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringBootCrudTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
